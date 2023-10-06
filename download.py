e4d223aa7e9995f2ec13709c06bba38b4f8b27b9
Alpha Hinex <AlphaHinex@gmail.com>
2023-10-06 10:05:12 +0800

    Try login hf

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 # Download a single file
 from huggingface_hub import hf_hub_download
-hf_hub_download(repo_id="tiiuae/falcon-7b-instruct", filename="pytorch_model-00002-of-00002.bin")
+hf_hub_download(repo_id="bigcode/starcoderbase-3b", filename="config.json")
 
 # Or an entire repository
 # from huggingface_hub import snapshot_download
