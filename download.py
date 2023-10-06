3f59e0c5e83fb76a3393e7b916c1e67f33a0467f
Alpha Hinex <AlphaHinex@gmail.com>
2023-10-06 09:09:42 +0800

    Another try

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 # Download a single file
-# from huggingface_hub import hf_hub_download
-# hf_hub_download(repo_id="tiiuae/falcon-7b-instruct", filename="config.json")
+from huggingface_hub import hf_hub_download
+hf_hub_download(repo_id="tiiuae/falcon-7b-instruct", filename="pytorch_model-00002-of-00002.bin")
 
 # Or an entire repository
-from huggingface_hub import snapshot_download
-snapshot_download("tiiuae/falcon-7b-instruct")
+# from huggingface_hub import snapshot_download
+# snapshot_download("tiiuae/falcon-7b-instruct")
