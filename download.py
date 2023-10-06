bb8af7c4e3baf5a6fc389a877c6b40f37d48d963
Alpha Hinex <AlphaHinex@gmail.com>
2023-10-06 11:01:27 +0800

    Starcoderbase-3b

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 # Download a single file
-from huggingface_hub import hf_hub_download
-hf_hub_download(repo_id="bigcode/starcoderbase-3b", filename="config.json")
+# from huggingface_hub import hf_hub_download
+# hf_hub_download(repo_id="tiiuae/falcon-7b-instruct", filename="config.json")
 
 # Or an entire repository
-# from huggingface_hub import snapshot_download
-# snapshot_download("tiiuae/falcon-7b-instruct", cache_dir="/home/runner/work/hf-models/hf-models")
+from huggingface_hub import snapshot_download
+snapshot_download("bigcode/starcoderbase-3b")
