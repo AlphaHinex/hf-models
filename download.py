f7cc87322d769f90e4ef1270f3dcb9c631c18d52
Alpha Hinex <AlphaHinex@gmail.com>
2023-10-11 16:26:15 +0800

    Test

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 # Download a single file
-# from huggingface_hub import hf_hub_download
-# hf_hub_download(repo_id="tiiuae/falcon-7b-instruct", filename="config.json")
+from huggingface_hub import hf_hub_download
+hf_hub_download(repo_id="tiiuae/falcon-7b-instruct", filename="config.json")
 
 # Or an entire repository
-from huggingface_hub import snapshot_download
-snapshot_download("bigcode/starcoderbase-7b")
+# from huggingface_hub import snapshot_download
+# snapshot_download("bigcode/starcoderbase-7b")
