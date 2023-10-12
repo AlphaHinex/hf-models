fd399f205985ff46d989ffeb3ceb7c8fffe9b845
Alpha Hinex <AlphaHinex@gmail.com>
2023-10-12 11:20:49 +0800

    Startcoder part 1

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:3-alpine
 
 COPY TONEK_FILE TONEK_FILE
-RUN pip install huggingface_hub
+RUN pip install huggingface_hub hf_transfer
 RUN huggingface-cli login --token | cat TONEK_FILE
 
 COPY download.py .
-RUN python download.py
+RUN HF_HUB_ENABLE_HF_TRANSFER=1 python download.py
 RUN rm -f /root/.cache/huggingface/token && rm -f TONEK_FILE
--- download.py
+++ download.py
@@ -1,9 +1,9 @@
 # Download a single file
-from huggingface_hub import hf_hub_download
-hf_hub_download(repo_id="tiiuae/falcon-7b-instruct", filename="config.json")
+# from huggingface_hub import hf_hub_download
+# hf_hub_download(repo_id="tiiuae/falcon-7b-instruct", filename="config.json")
 
 # Or an entire repository
-# from huggingface_hub import snapshot_download
-# snapshot_download("bigcode/starcoderbase-7b")
+from huggingface_hub import snapshot_download
+snapshot_download("bigcode/starcoder", ignore_patterns=["pytorch_model-00004-of-00007.bin", "pytorch_model-00005-of-00007.bin", "pytorch_model-00006-of-00007.bin"])
 
 # See more at https://huggingface.co/docs/huggingface_hub/en/guides/download
