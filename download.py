d94a4d03ccc08128a2038c267d4db6995348a843
Alpha Hinex <AlphaHinex@gmail.com>
2023-11-02 19:35:57 +0800

    Try snapshot_download

--- download.py
+++ download.py
@@ -1,12 +1,12 @@
 # Download a single file
-from huggingface_hub import hf_hub_download
+# from huggingface_hub import hf_hub_download
 # hf_hub_download(repo_id="tiiuae/falcon-7b-instruct", filename="config.json")
 
 # Or an entire repository
-# from huggingface_hub import snapshot_download
+from huggingface_hub import snapshot_download
 # snapshot_download("defog/sqlcoder")
 
 # Download from a dataset
-hf_hub_download(repo_id="bigcode/starcoderdata", repo_type="dataset", allow_patterns=["git-commits-cleaned/train-00000-of-00055.parquet", "github-issues-filtered-structured/train-00000-of-00059.parquet"])
+snapshot_download(repo_id="bigcode/starcoderdata", repo_type="dataset", allow_patterns=["git-commits-cleaned/train-00000-of-00055.parquet", "github-issues-filtered-structured/train-00000-of-00059.parquet"])
 
 # See more at https://huggingface.co/docs/huggingface_hub/en/guides/download
